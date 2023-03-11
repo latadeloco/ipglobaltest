5ce012d7cea2fbb36a26b95bd3128e9f788d0062
Jesús Robles Sánchez <jesusroblessanchez@gmail.com>
2023-03-11 21:06:10 +0100

    🔵 Added PostDto

--- src/Article/Post/Application/Query/PostDto.php
+++ src/Article/Post/Application/Query/PostDto.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 
 namespace App\Article\Post\Application\Query;
 
+use App\Article\Post\Domain\Post;
+
 final class PostDto
 {
+    public static function fromPost(Post $post): self
+    {
+        return new self(
+            title: $post->title(),
+            body: $post->body()
+        );
+    }
 
+    private function __construct(
+        public string $title,
+        public string $body
+    )
+    {
+    }
 }
